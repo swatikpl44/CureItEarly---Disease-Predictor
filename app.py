31f61eb67ac1fe9f040f55dad14784e21fad8f6a
Swatik <swatikpl44@gmail.com>
2021-04-01 12:29:42 +0530

    Merge ML model with flask

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import pandas as pd
+import numpy as np
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+test=pd.read_csv("test_data.csv",error_bad_lines=False)
+x_test=test.drop('prognosis',axis=1)
 
 @app.route('/')
-def hello():
+def about():
   return render_template("index.html")
 
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    if request.method=='POST':
+        col=x_test.columns
+        inputt = [str(x) for x in request.form.values()]
+
+        b=[0]*132
+        for x in range(0,132):
+            for y in inputt:
+                if(col[x]==y):
+                    b[x]=1
+        b=np.array(b)
+        b=b.reshape(1,132)
+        prediction = model.predict(b)
+        prediction=prediction[0]
+    return render_template('index.html', pred="The probable diagnosis says it could be {}".format(prediction))
 
 if __name__ == '__main__':
   app.run(debug = True)
